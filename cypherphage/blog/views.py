045e326034a5a4bfef207f916334a22bcd99e19b
Aditya Nain <cypherphage@gmail.com>
2022-03-10 18:06:59 +0530

    added models to admin panel

--- cypherphage/blog/views.py
+++ cypherphage/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+
+posts = [
+    {
+        'author': 'Cypher Phage',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 9, 2022'
+    },
+    {
+        'author': 'Aditya Nain',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 10, 2022'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
